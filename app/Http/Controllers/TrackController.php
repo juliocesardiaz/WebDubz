1b5a1ef52962ea8b027714839c309a177b466a84
Julio C. Diaz <juliocdiazramirez@gmail.com>
2015-12-03 14:35:43 -0800

    small changes

--- app/Http/Controllers/TrackController.php
+++ app/Http/Controllers/TrackController.php
@@ -14,9 +14,16 @@ class TrackController extends Controller
       */
       public function store(Request $request)
       {
+          $this->validate($request, [
+              'title' => 'required|max:255',
+              'artist' => 'required|max:255',
+              'track' => 'required',
+          ]);
+          
           $track = $request->file('track');
+          
           if ($track->isValid()) {
-              
+              //save file and create track model
           }
       }
 }
