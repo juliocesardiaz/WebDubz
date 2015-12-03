d7a92a48cd4abb794103526c6f7c834a70bea8e3
Julio C. Diaz <juliocdiazramirez@gmail.com>
2015-12-03 11:11:08 -0800

    defines attributes that are mass assignable in Track model

--- app/Track.php
+++ app/Track.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Track extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+     protected $fillable = ['title', 'artist', 'path_lq', 'path_hq', 'current_downloads'];
 }
