4e88c553ce06f1088e39718ea0850ffb2986a895
Julio C. Diaz <juliocdiazramirez@gmail.com>
2015-12-03 11:49:06 -0800

    adds template for webpages

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Webdubz</title>
+        <link rel="stylesheet" href="css/skeleton-2.0.4/css/skeleton.css">
+		<link rel="stylesheet" href="css/skeleton-2.0.4/css/normalize.css">
+    </head>
+    
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="twelve columns">
+                    <h1>Webdubz</h1>
+                </div>
+            </div>
+            @yield('content')
+        </div>
+    </body>
+</html>
