2084dc24b2b9835bc8d7e1aa0d7954dae019465b
Julio <jcdiaz92@Julios-MacBook-Pro.local>
2015-12-04 01:57:08 -0800

    fixes layout of tracks page

--- resources/views/tracks.blade.php
+++ resources/views/tracks.blade.php
@@ -6,31 +6,34 @@
         <!-- Display Validation Errors -->
         @include('common.errors')
         
+        <div class="twelve columns">
+            <form action="/upload">
+                <input type="submit" value="Upload a Track">
+            </form>
+        </div>
+            
         @if (count($tracks) > 0)
             @foreach ($tracks as $track)
-                <div class="three columns">
-                    <h5>{{ $track->title }} - {{ $track->artist }}</h5>
-                </div>
-                <div class="six columns" id="{{ $track->title }}">
-                    <audio controls preload="metadata">
-                        <source src={{ $track->path_lq }} type="audio/mpeg">
-                        Your browser does not support the audio tag.
-                    </audio>
-                </div>
-                <div class="three columns">
-                    <form action="/dubz/dowload/{{ $track->id }}">
-                        <input type="submit" value="Download">
-                    </form>
+                <div class="row">
+                    <div class="three columns">
+                        <h5>{{ $track->title }} - {{ $track->artist }}</h5>
+                    </div>
+                    <div class="six columns" id="{{ $track->title }}">
+                        <audio controls preload="metadata">
+                            <source src={{ $track->path_lq }} type="audio/mpeg">
+                            Your browser does not support the audio tag.
+                        </audio>
+                    </div>
+                    <div class="three columns">
+                        <form action="/dubz/dowload/{{ $track->id }}">
+                            <input type="submit" value="Download">
+                        </form>
+                    </div>
                 </div>
             @endforeach
         @else
             <div class="twelve columns">
                 <h3>No Dubz have been Uploaded</h3>
-            </div>
-            <div class="twelve columns">
-                <form action="/upload">
-                    <input type="submit" value="Upload a Track">
-                </form>
             </div>
         @endif
 @endsection
