b99e2b4cb961056344662222d157e239c14c9d98
Julio <jcdiaz92@Julios-MacBook-Pro.local>
2016-02-28 22:24:55 -0800

    small changes to api

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,6 +17,7 @@ class Cors
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
-            ->header('Acess-Control-Allow-Methods', 'GET');
+            ->header('Acess-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type');
     }
 }
