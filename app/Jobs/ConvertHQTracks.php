ab84bf4d5c2541819c787a9a2e35332da1afba33
juliocesardiaz <juliocdiazramirez@gmail.com>
2016-03-03 16:00:58 -0500

    small changes for server

--- app/Jobs/ConvertHQTracks.php
+++ app/Jobs/ConvertHQTracks.php
@@ -27,7 +27,7 @@ class ConvertHQTracks extends Job implements SelfHandling
      */
     public function handle()
     {
-        $command = '/usr/local/bin/ffmpeg -i  '. public_path() . $this->track->path_hq . ' -vn -ar 44100 -ac 2 -ab 128k -f mp3 ' . public_path() . $this->track->path_lq;
+        $command = '/usr/bin/ffmpeg -i  '. public_path() . $this->track->path_hq . ' -vn -ar 44100 -ac 2 -ab 128k -f mp3 ' . public_path() . $this->track->path_lq;
         $convert = new Process($command);
         $convert->run();
         $this->track->converted = 1;
