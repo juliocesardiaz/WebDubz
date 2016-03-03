39270d857a97fb99686ac9f483919c07193b0e62
Julio C. Diaz <juliocdiazramirez@gmail.com>
2016-03-02 16:41:21 -0800

    removes CheckDownload middle ware because it's unecesary

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -31,6 +31,5 @@ class Kernel extends HttpKernel
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'cors' => \App\Http\Middleware\Cors::class,
-        'check.download' => \App\Http\Middleware\CheckDownload::class,
     ];
 }
